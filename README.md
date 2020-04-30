7b8b3c4802fa53bc3d63266c8cb1934547c917de
Kamran Ali <kamranalinitb@gmail.com>
2020-04-30 06:48:17 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NYC taxi Data Analysis
+
+# Analysis
+- Which zones have the most pickup / drop offs overall
+- What are the peak hours for taxi
+- How are the trips distributed ? Why are people taking the cab?
+- What are the peak hours for long / short trips?
+- What are the top 3 pick up an drop off zones for long / short trips ?
+- How are people paying for the rides, on long / short trips
+- How is the payment tupe evolving with time?
+- Can we explore a ride - sharing opportunity by grouping close short trips?
+
+
+# Data Sources
+- [www1.nyc.gov](https://www1.nyc.gov/site/tlc/about/tlc-trip-record-data.page)
+- [academictorrents.com/](http://academictorrents.com/details/4f465810b86c6b793d1c7556fe3936441081992e)
+
+## Data Size
+
+- ~ 1.4 billion taxi rides between 2009 and 2016
+- ~ 400 GB uncompressed CSV
+- ~ 35 GB snappy parquet
---
+++ src/main/scala/atech/guide/TaxiApplication.scala
@@ -0,0 +1,56 @@
+package atech.guide
+
+import org.apache.spark.sql.SparkSession
+
+object TaxiApplication extends App {
+
+  private val spark = SparkSession
+    .builder()
+    .appName(getClass.getSimpleName)
+    .master("local[2]")
+    .getOrCreate()
+
+  spark.sparkContext.setLogLevel("ERROR")
+
+  val taxiDF = spark.read
+    .load("src/main/resources/data/yellow_taxi_jan_25_2018")
+
+  /**
+    * root
+    * |-- VendorID: integer (nullable = true)
+    * |-- tpep_pickup_datetime: timestamp (nullable = true) [Pickup Timestamp]
+    * |-- tpep_dropoff_datetime: timestamp (nullable = true) [drop off Timestamp]
+    * |-- passenger_count: integer (nullable = true)
+    * |-- trip_distance: double (nullable = true) [length of trip in miles]
+    * |-- RatecodeID: integer (nullable = true) [ 1 (Standard), 2 (JFK), 3 (Newark), 4 (Nassau/Westchester), 5 (negotiated)]
+    * |-- store_and_fwd_flag: string (nullable = true)
+    * |-- PULocationID: integer (nullable = true) [Pickup location zone ID]
+    * |-- DOLocationID: integer (nullable = true) [Drop off location zone ID]
+    * |-- payment_type: integer (nullable = true) [ 1 (credit card), 2(cash), 3(no charge), 4 (dispute), 5 (unknown), 6 (voided)]
+    * |-- fare_amount: double (nullable = true)
+    * |-- extra: double (nullable = true)
+    * |-- mta_tax: double (nullable = true)
+    * |-- tip_amount: double (nullable = true)
+    * |-- tolls_amount: double (nullable = true)
+    * |-- improvement_surcharge: double (nullable = true)
+    * |-- total_amount: double (nullable = true)
+    */
+
+  // taxiDF.printSchema
+  // println(taxiDF.count) // 331893
+
+  val taxiZoneDF = spark.read
+    .option("inferSchema", "true")
+    .option("header", "true")
+    .csv("src/main/resources/data/taxi_zones.csv")
+
+  /**
+    * root
+    * |-- LocationID: integer (nullable = true)
+    * |-- Borough: string (nullable = true)
+    * |-- Zone: string (nullable = true)
+    * |-- service_zone: string (nullable = true)
+    */
+  taxiZoneDF.printSchema()
+
+}
